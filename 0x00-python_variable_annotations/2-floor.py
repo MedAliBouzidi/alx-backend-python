4058c45604053cb17d029eb972b403b1b4af67c9
BOUZIDI Mohamed Ali <bouzidi.medali20@gmail.com>
2024-04-18 16:41:44 +0200

    task 2

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+""" This module contains a function that return the floor of a float. """
+import math
+
+
+def floor(n: float) -> int:
+    """This function returns the floor of a float."""
+    return math.floor(n)
