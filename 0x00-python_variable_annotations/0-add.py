1653a442c8b99515f76a3e396b7cecc34975e915
BOUZIDI Mohamed Ali <bouzidi.medali20@gmail.com>
2024-04-18 16:38:21 +0200

    task 0

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+""" This module contains a function that adds two numbers. """
+
+
+def add(a: float, b: float) -> float:
+    """This function adds two numbers."""
+    return a + b
